878b4deeb5416aa4cf02f7aeae3171e0d7855d94
Mathmb1986 <52980023+mathmb1986@users.noreply.github.com>
2025-11-01 01:02:12 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,4 @@
 -- server.lua
-local OLLAMA_URL = "192.168.1.2:30768"  -- ex: http://192.168.1.10  -- Caddy
-
 Config = {
   ResourceName = 'IAManager-ValStudios', -- mets EXACTEMENT le nom du dossier ressource
   OllamaURL    = 'http://192.168.1.2:30768',
