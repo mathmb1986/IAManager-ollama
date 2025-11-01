a033bcd3ac44e2fc985d906fcb1cefa42bc06901
Mathmb1986 <52980023+mathmb1986@users.noreply.github.com>
2025-11-01 03:15:43 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 Config = {
-  ResourceName = 'IAManager-ValStudios',-- mets EXACTEMENT le nom du dossier ressource
+  ResourceName = 'IAManager-ollama',-- mets EXACTEMENT le nom du dossier ressource
   OllamaURL    = 'http://192.168.1.2:30768', -- ex: http://192.168.1.10  -- Caddy
   Model        = 'artifish/llama3.2-uncensored',
 
-  -- limites & sécurité
+  -- limites & sÃ©curitÃ©
   MaxSteps     = 3,
   MinReward    = 250,
   MaxReward    = 25000,
